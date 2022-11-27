36895cb3e5c585ca296c39685a6bb15da78099d6
fragilebanana16 <850604714@qq.com>
2022-11-27 22:24:40 +0800

    登录界面默认值

--- WpfApp/obj/Debug/WpfApp_Content.g.i.cs
+++ WpfApp/obj/Debug/WpfApp_Content.g.i.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/close.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/minus.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/key.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/user.png")]
 
 
